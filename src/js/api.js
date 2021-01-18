91aeeec829cb2cfd6901804bda9c82d1dc98bd34
marcelarie <googlillo@gmail.com>
2021-01-18 13:15:31 +0100

    Adding jQuery

--- src/js/api.js
+++ src/js/api.js
@@ -1,14 +1,17 @@
 
-$.ajax({
-    url: 'https://itunes.apple.com/search?term=rihanna',
-    type: 'GET',
-    dataType: 'xml/html/script/json',
-    success: function (data, textStatus, jqXHR) {
-        console.log(data)
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-        // error callback
-    }
-});
+function getRihanna() {
+    $.ajax({
+        url: 'https://itunes.apple.com/search?term=rihanna',
+        type: 'GET',
+        dataType: 'xml/html/script/json',
+        success: function (data, textStatus, jqXHR) {
+            console.log(data)
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            // error callback
+        }
+    });
+}
 
 
+export {getRihanna}
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import {getRihanna} from './api.js'
+
+getRihanna()
