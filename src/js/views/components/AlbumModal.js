97094b51c0a1a909117e45b8608315974b9d1edc
Marcel <googlillo@gmail.com>
2021-01-20 22:06:08 +0100

    Added style albumModal

--- src/js/views/components/AlbumModal.js
+++ src/js/views/components/AlbumModal.js
@@ -1,3 +1,4 @@
+
 const collection = ({
     artworkUrl100 = 'https://findicons.com/files/icons/2770/ios_7_icons/100/music.png',
     collectionName,
@@ -6,21 +7,29 @@ const collection = ({
     trackCount,
     releaseDate,
     primaryGenreName,
+    currency
 }) => {
+    const date = new Date(releaseDate).toLocaleDateString()
     const template = `
   <section class="modal-type">
     <section class="modal-type__content">
       <section class="modal-type-content__cover">
+    <br>
           <img src="${artworkUrl100}" alt="${artistName}">
-          <h2>${collectionName}</h2>
-          <h3>${artistName}</h3>
-        <p>Track Count: ${trackCount}</p>
-        <p>Musical genre: ${primaryGenreName}</p>
-        <p>Release date: ${releaseDate}</p>
-        <p>Song Price: ${collectionPrice}</p>
+    <br>
+          <h2>${collectionName}
       <span class="material-icons starred"> 
           star_border 
       </span>
+    </h2>
+          <h3>${artistName}</h3>
+    <section class="modal-type-content__info">
+        <br>
+        <p>Track Count: <strong>${trackCount}</strong></p>
+        <p>Musical genre: <strong>${primaryGenreName}</strong></p>
+        <p>Release date: <strong>${date}</strong></p>
+        <p>Song Price: <strong>${collectionPrice} ${currency}</strong></p>
+      </section>
       </section>
     </section>
   </section>
